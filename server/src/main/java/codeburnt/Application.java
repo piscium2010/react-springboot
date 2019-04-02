63b1d26e91f7d71ec674404971c1d61d65763129
Xiaobin <xshen1@statestreet.com>
2019-04-02 10:14:56 +0800

    sp

--- server/src/main/java/codeburnt/Application.java
+++ server/src/main/java/codeburnt/Application.java
@@ -1,16 +1,13 @@
-package hello;
+package codeburnt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-@Controller
-public class Application extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer{
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
@@ -18,10 +15,5 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @RequestMapping(value = "/")
-    public String hello() {
-        return "Hello World from Tomcat";
     }
 }
